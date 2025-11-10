05516ae125e02badc23ac4ea372090f8bd23501f
Ramilka <102136743+rgadrfa@users.noreply.github.com>
2025-11-10 20:21:55 +0500

    формирование KeyService, рефакторинг CryptoService

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,10 +2,6 @@ package app;
 
 import models.crypto.CryptoServiceFactory;
 import models.crypto.enums.AsymmetricEncoder;
-import models.crypto.enums.SymmetricEncoder;
-import models.crypto.enums.SymmetricKeyType;
-import models.crypto.enums.type.SymmetricTypeEncoder;
-import models.crypto.enums.mode.SymmetricModeEncoder;
 import models.key.KeyServiceFactory;
 import models.key.enums.AsymmetricKeyType;
 
@@ -15,8 +11,5 @@ import java.security.NoSuchAlgorithmException;
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchPaddingException {
         new Application();
-        var t = KeyServiceFactory.createAsymmetricKeyService(AsymmetricKeyType.RSA_1024);
-        var g = CryptoServiceFactory.createAsymmetric(AsymmetricEncoder.RSA_OAEPWithSHA1AndMGF1Padding);
-        g.setKeyService(t);
     }
 }
