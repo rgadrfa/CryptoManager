23f4626da8da3fc605e5a572babef0a30fab29ea
Ramilka <102136743+rgadrfa@users.noreply.github.com>
2025-11-14 14:48:37 +0500

    Создание модели файловой системы

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,16 +1,7 @@
 package app;
 
-import models.crypto.CryptoServiceFactory;
-import models.crypto.enums.SymmetricEncoder;
-import models.file.FileService;
-import models.key.KeyServiceFactory;
-import models.key.enums.SymmetricKeyType;
-
-import javax.crypto.NoSuchPaddingException;
-import java.security.NoSuchAlgorithmException;
-
 public class Main {
-    public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException {
+    public static void main(String[] args){
         new Application();
     }
 }
