32d1ec4a5c3b53d2261c9fcc178a8b122a147e96
Ramilka <102136743+rgadrfa@users.noreply.github.com>
2025-11-14 14:48:28 +0500

    Создание модели файловой системы

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -11,13 +11,6 @@ import java.security.NoSuchAlgorithmException;
 
 public class Main {
     public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException {
-        var fileService = new FileService();
-
-
-        var cryptoService = CryptoServiceFactory.create(SymmetricEncoder.AES_ECB_ISO10126Padding);
-        var keyService = KeyServiceFactory.create(SymmetricKeyType.AES_128);
-        var key = keyService.getKeyController().createKey();
-
-        fileService.encrypt(cryptoService, key);
+        new Application();
     }
 }
